d17a43b83bd9fc553ddfe1415d3ab41239b5a85c
romanleonidov <roman.pozdnykow@mail.ru>
2018-07-14 22:05:05 +0300

    Вывод постов

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
       <div class="row">
 
         <div class="col-sm-8 blog-main">
-
+					<?php 
+						if (have_posts() ) : while (have_posts() ) : the_post();
+						get_template_part('content', get_post_format() );
+						endwhile; endif;
+					?>
           
 
           
