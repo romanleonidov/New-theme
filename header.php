192937c76c902f24a68b030e693e8ea835015bc0
romanleonidov <roman.pozdnykow@mail.ru>
2018-07-14 22:30:05 +0300

    Вывод страницы

--- header.php
+++ header.php
@@ -36,11 +36,7 @@
     <div class="blog-masthead">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
+          <a class="blog-nav-item active" href="#"><?php wp_list_pages('&title_li=' ); ?></a>
         </nav>
       </div>
     </div>
