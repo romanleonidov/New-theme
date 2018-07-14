5b2dfd1c40d99c418dc644dcf2bf828694c71d8f
romanleonidov <roman.pozdnykow@mail.ru>
2018-07-14 22:17:41 +0300

    Вывод постов

--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <div class="blog-post">
-  <h2 class="blog-post-title"><?php the_title(); ?>/h2>
+  <h2 class="blog-post-title"><?php the_title(); ?></h2>
   <p class="blog-post-meta"><?php the_date(); ?> <a href="#"><?php the_author(); ?></a></p>
 
    <?php the_content(); ?>        
